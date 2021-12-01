d8c26088bef79636b2d5e8de92713c6e0f57855e
Spencer Williams <spencerwi@gmail.com>
2021-12-01 20:24:08 +0000

    Use Seqs for better perf

--- day1/Program.fs
+++ day1/Program.fs
@@ -1,23 +1,26 @@
+#nowarn "25"
+
 // Utility function to perform the repeated task of counting
 //  how many times within a list of ints a particular element
 //  is larger than its predecessor, since both parts of this
 //  problem require that.
-let count_increases (input : int array) =
+let count_increases (input : int seq) =
     input
-    |> Array.pairwise
-    |> Array.filter (fun (prev, current) -> current > prev)
-    |> Array.length
+    |> Seq.pairwise
+    |> Seq.filter (fun (previous, current) -> current > previous)
+    |> Seq.length
 
 // Part 1: how often is the current number larger than its predecessor?
 let part_a = count_increases 
 
 // Part 2: how often is the current three-number-window-sum larger than 
 //  the previous three-number-window-sum?
-let part_b (input: int array) =
-    let sliding_sum_list =
-        input 
-        |> Array.windowed 3
-        |> Array.map Array.sum
+let part_b (input: int seq) =
+    let sliding_sum_list = 
+        seq {
+            for [|a;b;c|] in (Seq.windowed 3 input) do
+                yield a+b+c
+        } 
     in
     count_increases sliding_sum_list
     
@@ -26,7 +29,8 @@ let part_b (input: int array) =
 let main argv =
     let input = 
         System.IO.File.ReadAllLines "input.txt" 
-        |> Array.map int
+        |> Seq.ofArray
+        |> Seq.map int
     in
     printfn "Part A: %d" (part_a input);
     printfn "Part B: %d" (part_b input);
