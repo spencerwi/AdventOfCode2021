2c8fa7e22b26cf0b6ccb7554e7e6af35acc4ef1d
Spencer Williams <spencerwi@gmail.com>
2021-12-02 19:25:30 +0000

    Use records not tuples

--- day2/Program.fs
+++ day2/Program.fs
@@ -1,41 +1,42 @@
 ﻿open System.Text.RegularExpressions
 
-type Point = int * int // (x, y)
-type SubStatus = Point * int //(position, aim)
+type Point = {x: int; y: int}
+type SubStatus = {x: int; y: int; aim: int}
 
 module Instruction = begin
     type t = 
-        Forward of int
+        | Forward of int
         | Down of int
         | Up of int
 
     let parse (line: string) =
         let pattern = "(?<command>forward|up|down) (?<amount>\d+)"
         let matched = Regex.Match(line, pattern)
-        match matched.Groups["command"].Value, matched.Groups["amount"].Value with
-        | "forward", amount -> Forward (int amount)
-        | "up", amount -> Up (int amount)
-        | "down", amount -> Down (int amount)
-        | other, _ -> failwith ("Unrecognized command: " + other)
+        let amount = int matched.Groups["amount"].Value 
+        match matched.Groups["command"].Value with
+        | "forward" -> Forward amount
+        | "up" -> Up amount
+        | "down" -> Down amount
+        | other -> failwith ("Unrecognized command: " + other)
 
-    let move ((current_x, current_y): Point) = function
-        | Forward x -> (current_x + x, current_y)
-        | Down y -> (current_x, current_y + y) // We increase our "y" as we go *down* for this puzzle
-        | Up y -> (current_x, current_y - y) // We increase our "y" as we go *down* for this puzzle
+    let move (current: Point) = function
+        | Forward x -> {current with x = current.x + x}
+        | Down y -> {current with y = current.y + y} // We increase our "y" as we go *down* for this puzzle
+        | Up y -> {current with y = current.y - y} // We increase our "y" as we go *down* for this puzzle
 
-    let move_with_aim (((current_x, current_y), current_aim): SubStatus) = function 
-        | Forward x -> ((current_x + x, current_y + (current_aim * x)), current_aim)
-        | Down y -> ((current_x, current_y), current_aim + y)
-        | Up y -> ((current_x, current_y), current_aim - y)
+    let move_with_aim (current: SubStatus) = function 
+        | Forward x -> {current with x = current.x + x; y = current.y + (current.aim * x)}
+        | Down y -> {current with aim = current.aim + y}
+        | Up y -> {current with aim = current.aim - y}
 end
 
 let part_a (input: Instruction.t seq) = 
-    let final_x, final_y = Seq.fold Instruction.move (0,0) input
-    in final_x * final_y
+    let final = Seq.fold Instruction.move {x=0; y=0} input 
+    in final.x * final.y
 
 let part_b (input: Instruction.t seq) =
-    let (final_x, final_y), _ = Seq.fold Instruction.move_with_aim ((0,0), 0) input 
-    in final_x * final_y
+    let final = Seq.fold Instruction.move_with_aim {x=0; y=0; aim=0} input 
+    in final.x * final.y
 
 [<EntryPoint>]
 let main argv =
