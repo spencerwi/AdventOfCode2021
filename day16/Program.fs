def2563df8f79dd0f3be2af83da4a97b390edaaf
Spencer Williams <spencerwi@gmail.com>
2021-12-16 21:50:19 +0000

    Fix incorrect offsets after literals (advance by the WHOLE section length, not just the 4-bits-in-each-group length)

--- day16/Program.fs
+++ day16/Program.fs
@@ -24,24 +24,31 @@ module Packets = begin
             let typeId = Convert.ToInt32(bits.Substring(3, 3), 2) in
             match typeId with
             | 4 -> 
-                let allButLastGroup = 
-                    bits[6..] 
-                    |> Seq.chunkBySize 5
-                    |> Seq.takeWhile (fun chunk -> chunk[0] <> '0') 
-                    |> Seq.map (fun chars -> new String(chars[1..]))
+                let literalValueSection =
+                    let allButLastGroup = 
+                        bits[6..]
+                        |> Seq.chunkBySize 5
+                        |> Seq.takeWhile (fun group -> group[0] <> '0')
+                    let lastGroup = 
+                        bits[6..]
+                        |> Seq.chunkBySize 5
+                        |> Seq.skipWhile (fun group -> group[0] <> '0')
+                        |> Seq.head
+                    in seq {
+                        yield! allButLastGroup
+                        yield lastGroup
+                    }
+                let literalValueSectionLength =
+                    (Seq.length literalValueSection) * 5
+                let valueStr = 
+                    literalValueSection
+                    |> Seq.map (fun group -> group[1..])
+                    |> Seq.map (fun group -> new String(group))
                     |> String.concat ""
-                in
-                let lastGroup = 
-                    bits[6..]
-                    |> Seq.chunkBySize 5
-                    |> Seq.skipWhile(fun chunk -> chunk[0] <> '0')
-                    |> Seq.head
-                    |> (fun chars -> new String(chars[1..]))
-                let literalValueStr = allButLastGroup + lastGroup in
-                let literalValue = Convert.ToInt64(literalValueStr, 2) in
-                printfn " Literal %d" literalValue
-                let totalPacketLength = 6 + literalValueStr.Length
-                Some (bits[totalPacketLength + 1..], Literal {version = version; value = literalValue})
+                let value = Convert.ToInt64(valueStr, 2)
+                printfn " Literal %d" value
+                let totalPacketLength = 6 + literalValueSectionLength
+                Some (bits[totalPacketLength..], Literal {version = version; value = value})
             | _ -> 
                 let length = 
                     match bits[6] with
@@ -52,35 +59,36 @@ module Packets = begin
                 let leftoverBits, subpackets =
                     match length with
                     | Characters count ->
-                        let mutable remainingBitsInPacket = bits.Substring(22, count) in
+                        let remainingBitsAfterThisPacket = bits.Substring(22 + count) in
+                        let mutable remainingBitsInThisPacket = bits.Substring(22, count) in
                         (
-                            bits.Substring(22 + count),
+                            remainingBitsAfterThisPacket,
                             [
                                 let mutable newIndent = indent + 22 
-                                while String.length remainingBitsInPacket > 0 do
-                                    match parsePacket newIndent remainingBitsInPacket with
+                                while String.length remainingBitsInThisPacket > 0 do
+                                    match parsePacket newIndent remainingBitsInThisPacket with
                                     | Some (unparsedBitsInPacket, subpacket) ->
-                                        newIndent <- newIndent + (remainingBitsInPacket.Length - unparsedBitsInPacket.Length)
-                                        remainingBitsInPacket <- unparsedBitsInPacket
+                                        newIndent <- newIndent + (remainingBitsInThisPacket.Length - unparsedBitsInPacket.Length)
+                                        remainingBitsInThisPacket <- unparsedBitsInPacket
                                         yield subpacket
-                                    | None -> remainingBitsInPacket <- ""
+                                    | None -> remainingBitsInThisPacket <- "" // stop looping
                             ]
                         )
                     | Packets count ->
-                        let mutable remainingBitsInPacket = bits[18..] in
+                        let mutable allRemainingBits = bits[18..] in
                         //printfn "Remaining bits in the packet: %s" remainingBitsInPacket
                         let subpacketList = [
                             let mutable newIndent = indent + 18
                             for _ in 1..count do
-                                match parsePacket newIndent remainingBitsInPacket with
+                                match parsePacket newIndent allRemainingBits with
                                 | Some (unparsedBitsFromPacket, subpacket) ->
-                                    newIndent <- newIndent + (remainingBitsInPacket.Length - unparsedBitsFromPacket.Length)
-                                    remainingBitsInPacket <- unparsedBitsFromPacket
+                                    newIndent <- newIndent + (allRemainingBits.Length - unparsedBitsFromPacket.Length)
+                                    allRemainingBits <- unparsedBitsFromPacket
                                     yield subpacket
                                 | None -> ()
                         ] in
                         //printfn "Remaining bits in the packet: %s" remainingBitsInPacket
-                        (remainingBitsInPacket, subpacketList)
+                        (allRemainingBits, subpacketList)
                 in
                 Some (leftoverBits, Operator {version = version; length = length; subpackets = subpackets})
 
@@ -127,7 +135,6 @@ let main argv =
         |> Packets.hexadecimalToBits
         |> Packets.parsePacket 0 
         |> Option.get
-    printfn "%A" rootPacket
     printfn "Part A: %d" (Packets.versionSum rootPacket)
     // printfn "Part A: %d" (partA input)
     0
